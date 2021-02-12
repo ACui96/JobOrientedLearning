42335d44f4ce1f33e8872ffbe5851615013c4acb
acui <acui962018@gmail.com>
2021-02-12 13:43:52 +0800

    队列的最大值

--- src/ADataStructure/Code/A8MaxQueue.java
+++ src/ADataStructure/Code/A8MaxQueue.java
@@ -5,7 +5,28 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
+ *剑指 Offer 59 - II. 队列的最大值
+ * 请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
  *
+ * 若队列为空，pop_front 和 max_value 需要返回 -1
+ *
+ * 示例 1：
+ *
+ * 输入:
+ * ["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+ * [[],[1],[2],[],[],[]]
+ * 输出: [null,null,null,2,1,2]
+ * 示例 2：
+ *
+ * 输入:
+ * ["MaxQueue","pop_front","max_value"]
+ * [[],[],[]]
+ * 输出: [null,-1,-1]
+ *
+ * 作者：Krahets
+ * 链接：https://leetcode-cn.com/leetbook/read/illustration-of-algorithm/e2t5ug/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class A8MaxQueue {
     Queue<Integer> queue;
