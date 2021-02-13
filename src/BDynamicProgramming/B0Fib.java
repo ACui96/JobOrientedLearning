cabf12101ad6d64847e6593ef59784770ed9904c
acui <acui962018@gmail.com>
2021-02-13 11:29:43 +0800

    斐波那契

--- src/BDynamicProgramming/B0Fib.java
+++ src/BDynamicProgramming/B0Fib.java
@@ -1,5 +1,20 @@
 package BDynamicProgramming;
 
+/**
+ * 剑指 Offer 10- I. 斐波那契数列
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+ *
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ *
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * 作者：Krahets
+ * 链接：https://leetcode-cn.com/leetbook/read/illustration-of-algorithm/50fxu1/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
 public class B0Fib {
     public static void main(String[] args) {
         int n = 111111111;
