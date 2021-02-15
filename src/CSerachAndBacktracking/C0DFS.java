74404c5d870fc90c18b51ae119f7626931c0dbff
acui <acui962018@gmail.com>
2021-02-15 10:43:53 +0800

    矩阵中的路径

---
+++ src/CSerachAndBacktracking/C0DFS.java
@@ -0,0 +1,38 @@
+package CSerachAndBacktracking;
+
+public class C0DFS {
+    public static void main(String[] args) {
+        char[][] board = new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word = "ABCCED";
+        boolean res = exist(board, word);
+        System.out.println(res);
+    }
+
+    public static boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (dfs(board, words, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean dfs(char[][] board, char[] words, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != words[k]) {
+            return false;
+        }
+        if (k == words.length - 1) {
+            return true;
+        }
+        board[i][j] = '\0';
+        boolean res = dfs(board, words, i + 1, j, k + 1) || dfs(board, words, i - 1, j, k + 1) ||
+                dfs(board, words, i, j + 1, k + 1) || dfs(board, words, i, j - 1, k + 1);
+        board[i][j] = words[k];
+        return res;
+    }
+}
